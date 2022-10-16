5ed2812d596e681922f40685f38facfe9d4dfc92
Broseki <broseki@cavefox.net>
2022-10-16 18:38:10 -0400

    Added server player number check

--- Controllers/ServerListenerController.cpp
+++ Controllers/ServerListenerController.cpp
@@ -4,7 +4,10 @@
 
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 #include "ServerListenerController.h"
+
+#define SERVER_FULL_MESSAGE "Server is full. Please try again later."
 
 ServerListenerController::ServerListenerController(LogController *log_controller,
                                                    ServerSocketModel *server_socket_model,
@@ -50,7 +53,21 @@ void ServerListenerController::start() {
 
         // Lock the sessions mutex and add the new session
         this->sessions->sessions_mutex.lock();
-        this->sessions->sessions.insert(std::pair<int, std::shared_ptr<SessionModel>>(client_socket, session));
+
+        // Make sure that the server has capacity for new connections
+        if (this->sessions->sessions.size() >= this->configuration_model->max_players) {
+            this->log_controller->log(LogController::LogLevel::INFO, "ServerListenerController - Server is full, closing connection from [" + std::string(inet_ntoa(address.sin_addr)) + "]");
+
+            // Send a message to the client that the server is full
+            std::string message = SERVER_FULL_MESSAGE;
+            message += "\n";
+            send(session->socketfd, message.c_str(), strlen(message.c_str()), 0);
+
+            // Close the connection
+            close(session->socketfd);
+        } else {
+            this->sessions->sessions.insert(std::pair<int, std::shared_ptr<SessionModel>>(client_socket, session));
+        }
 
         // Unlock the sessions mutex
         this->sessions->sessions_mutex.unlock();
