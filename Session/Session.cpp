688c86e2605b0b8f3f2cf36be90d4447ff5aa8ef
Broseki <broseki@cavefox.net>
2022-10-18 20:41:34 -0400

    Added basic session buffer read/write functions

--- Session/Session.cpp
+++ Session/Session.cpp
@@ -11,3 +11,29 @@ Session::Session(int socketfd) {
 int Session::get_socketfd() {
     return this->socketfd;
 }
+
+std::vector<char8_t> Session::get_input_buffer() {
+    const std::lock_guard<std::mutex> lock(this->input_buffer_mutex);
+
+    this->input_buffer.clear();
+    return this->input_buffer;
+}
+
+void Session::set_input_buffer(std::vector<char8_t> arg_input_buffer) {
+    const std::lock_guard<std::mutex> lock(this->input_buffer_mutex);
+
+    this->input_buffer = arg_input_buffer;
+}
+
+std::vector<char8_t> Session::get_output_buffer() {
+    const std::lock_guard<std::mutex> lock(this->output_buffer_mutex);
+
+    this->output_buffer.clear();
+    return this->output_buffer;
+}
+
+void Session::set_output_buffer(std::vector<char8_t> arg_output_buffer) {
+    const std::lock_guard<std::mutex> lock(this->output_buffer_mutex);
+
+    this->output_buffer = arg_output_buffer;
+}
--- Session/Session.h
+++ Session/Session.h
@@ -5,7 +5,7 @@
 #ifndef CAVEMUD_SESSION_H
 #define CAVEMUD_SESSION_H
 
-#include <string>
+#include <vector>
 #include <mutex>
 
 class Session {
@@ -14,11 +14,17 @@ public:
 
     int get_socketfd();
 
+    std::vector<char8_t> get_input_buffer();
+    void set_input_buffer(std::vector<char8_t> arg_input_buffer);
+
+    std::vector<char8_t> get_output_buffer();
+    void set_output_buffer(std::vector<char8_t> arg_output_buffer);
+
 private:
     // Data
     int socketfd;
-    std::string input_buffer_queue;
-    std::string output_buffer;
+    std::vector<char8_t> input_buffer;
+    std::vector<char8_t> output_buffer;
 
     // Mutexes
     std::mutex input_buffer_mutex;
